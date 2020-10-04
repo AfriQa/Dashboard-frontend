fb3988f128a1a5f92ca87323c2dba536f3a529fe
Abraham Terfie <abrahamterfie4563@gmail.com>
2020-10-04 17:55:12 +0300

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import { useRoutes } from "react-router-dom/Route";
+
 import "react-perfect-scrollbar/dist/css/styles.css";
+import React from "react";
+import  {useRoutes} from "react-router-dom/index";
 import { ThemeProvider } from "@material-ui/core";
 import GlobalStyles from "./Components/GlobalStyles/GlobalStyles";
 
-import "./Mixins/Charts"
+import "./Mixins/Charts";
 import theme from "./Theme";
 import routes from "./routes";
 
 
-
 const App = () => {
   const routing = useRoutes(routes);
+
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       {routing}
-      <GlobalStyles/>
     </ThemeProvider>
   );
 };
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,19 +4,32 @@ import DashboardLayout from "./Components/Layouts/DashBoard";
 import Account from "./Pages/Account/Index";
 import Customer from "./Pages/Customer";
 import DashboardView from "./Components/Layouts/DashBoard";
-//import SettingsView from "src/views/settings/SettingsView";
-//import ProductListView from "src/views/product/ProductListView";
+import SettingsView from "./Pages/Settings";
+import Products from "./Pages/Products";
+import MainLayout from "./Components/Layouts/MainLayout";
+
 const routes = [
   {
     path: "app",
     element: <DashboardLayout />,
     childern: [
       { path: "accounts", elements: <Account /> },
-      // { path: "customers", element: <CustomerListView /> },
+      { path: "customers", element: <Customer /> },
       { path: "dashboard", element: <DashboardView /> },
-      //   { path: "products", element: <ProductListView /> },
-  //    { path: "settings", element: <SettingsView /> },
-      //{ path: "*", element: <Navigate to="/404" /> },
+      { path: "products", element: <Products /> },
+      { path: "settings", element: <SettingsView /> },
+      { path: "*", element: <Navigate to="/404" /> },
+    ],
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+   // { path: "login", element: <LoginView /> },
+     // { path: "register", element: <RegisterView /> },
+      //{ path: "404", element: <NotFoundView /> },
+     // { path: "/", element: <Navigate to="/app/dashboard" /> },
+     // { path: "*", element: <Navigate to="/404" /> },
     ],
   },
 ];
