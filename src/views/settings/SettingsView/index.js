7281bc1bc1999dcd283eb4f8c57d66996fc39719
Abraham Terfie <abrahamterfie4563@gmail.com>
2020-10-17 06:26:02 +0300

    settings done

--- src/views/settings/SettingsView/index.js
+++ src/views/settings/SettingsView/index.js
@@ -10,21 +10,12 @@ import Notifications from './Notifications';
 import Password from './Password';
 import './settings.scss'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(5)
-  }
-}));
-
 const SettingsView = () => {
-  const classes = useStyles();
+
 
   return (
     <Page
-      className={classes.root}
+      className="indexRoot"
       title="Settings"
     >
       <Container maxWidth="lg">
