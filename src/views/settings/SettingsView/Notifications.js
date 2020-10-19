b3b7a9883e4523f42bf07897d7ac87423973b523
Abraham Terfie <abrahamterfie4563@gmail.com>
2020-10-19 05:05:13 +0300

    fixing imports

--- src/views/settings/SettingsView/Notifications.js
+++ src/views/settings/SettingsView/Notifications.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import {
-
-  FormControlLabel,
-  Grid,
- 
-
-} from '@material-ui/core';
-import Divider from '@material-ui/core/Divider'
-
-import Checkbox from '@material-ui/core/Checkbox'
-import CardHeader from '@material-ui/core/CardHeader'
-import Box from '@material-ui/core/Box'
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Checkbox from '@material-ui/core/Checkbox';
+import CardHeader from '@material-ui/core/CardHeader';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import { withStyles } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 import './settings.css'
