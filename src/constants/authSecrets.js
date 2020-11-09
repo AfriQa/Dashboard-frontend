65e428a302eeef820c1fcd814b0c5af696c084e8
async69 <yeabsera0830@gmail.com>
2020-11-09 18:07:43 +0300

    updated

--- src/constants/authSecrets.js
+++ src/constants/authSecrets.js
@@ -1,5 +1,5 @@
 export const TwilioCrerdentials = {
     username: "AC079869d4d9442856f4fc37b15a2382cd",
-    password: "3e854bdecba30a02f56254d75a4e4fbf",
+    password: "d9cb1fd763eae66d2fcb604577e0d4d2",
     serviceID: "VA44babdaaaa839dc4638d52111a739ac5"
 }
